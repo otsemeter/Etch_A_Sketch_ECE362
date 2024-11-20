61a44f4bce8d80437eb65662ab7325335e2732cc
nwisnie <nwisnie@ecelnx54.ecn.purdue.edu>
2024-11-20 14:38:06 -0500

    i2c stuff

--- src/main.c
+++ src/main.c
@@ -20,6 +20,13 @@ char* username = "osemeter";
 char matrix [64][25];
 int curX = 0;
 int curY = 0;
+void init_i2c();
+void enable_ports();
+void i2c_start(uint32_t targadr, uint8_t size, uint8_t dir);
+void i2c_stop();
+void i2c_waitidle();
+void i2c_clearnack();
+int i2c_checknack();
 void set_char_msg(int, char);
 void nano_wait(unsigned int);
 void game(void);
@@ -33,13 +40,239 @@ void autotest();
 void enable_ports(void) {
     // Only enable port C for the keypad
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
     GPIOC->MODER &= ~0xffff;
     GPIOC->MODER |= 0x55 << (4*2);
     GPIOC->OTYPER &= ~0xff;
     GPIOC->OTYPER |= 0xf0;
     GPIOC->PUPDR &= ~0xff;
     GPIOC->PUPDR |= 0x55;
+
+    GPIOA->MODER &= ~(GPIO_MODER_MODER11 | GPIO_MODER_MODER12);
+    GPIOA->MODER |= (GPIO_MODER_MODER11_1 | GPIO_MODER_MODER12_1);
+
+    GPIOA->AFR[1] &= ~0x000FF000;
+    GPIOA->AFR[1] |= 0x00055000;
 }
+
+
+
+// I2C stuff:
+
+void init_i2c(void) {
+    RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+
+    I2C2->CR1 &= ~I2C_CR1_PE; // I2C disabled
+    I2C2->CR1 |= I2C_CR1_ANFOFF;
+    I2C2->CR1 |= I2C_CR1_ERRIE;
+    // I2C2->CR1 |= I2C_CR1_ERRIE | I2C_CR1_NACKIE | I2C_CR1_STOPIE;
+    I2C2->CR1 |= I2C_CR1_NOSTRETCH;
+
+    // PRESC = 5 (divides 48 MHz by (PRESC + 1) to 8 MHz for I2CCLK)
+    // SCLL = 0x09 (for the low period of SCL clock)
+    // SCLH = 0x03 (for the high period of SCL clock)
+    // SDADEL = 0x01 (data delay after SDA rising edge)
+    // SCLDEL = 0x03 (delay between SDA and SCL change)
+    I2C2->TIMINGR = (5 << 28) | (0x03 << 20) | (0x09 << 0) | (0x01 << 16) | (0x03 << 8);
+
+    I2C2->CR2 &= ~I2C_CR2_ADD10;  // Set to 7-bit addressing mode
+    I2C2->CR2 &= ~I2C_CR2_AUTOEND; // Automatically send STOP condition after the last byte
+
+    I2C2->CR1 |= I2C_CR1_PE;
+}
+
+void i2c_start(uint32_t targadr, uint8_t size, uint8_t dir) {
+    uint32_t tmpreg = I2C2->CR2;
+
+    // Clear SADD, NBYTES, RD_WRN, START, STOP
+    tmpreg &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN | I2C_CR2_START | I2C_CR2_STOP);
+
+    // Set read/write direction in tmpreg.
+    if (dir == 1) {
+        tmpreg |= I2C_CR2_RD_WRN; 
+    } else {
+        tmpreg &= ~I2C_CR2_RD_WRN;
+    }
+
+    // 3. Set the target's address in SADD (shift targadr left by 1 bit) and the data size.
+    tmpreg |= ((targadr<<1) & I2C_CR2_SADD) | ((size << 16) & I2C_CR2_NBYTES);
+
+    // 4. Set the START bit.
+    tmpreg |= I2C_CR2_START;
+
+    // 5. Start the conversion by writing the modified value back to the CR2 register.
+    I2C2->CR2 = tmpreg;
+}
+
+void i2c_stop(void) {
+    // 0. If a STOP bit has already been sent, return from the function.
+    // Check the I2C2 ISR register for the corresponding bit.
+    if (I2C2->ISR & I2C_ISR_STOPF) {
+        return;
+    }
+
+    // 1. Set the STOP bit in the CR2 register.
+    I2C2->CR2 |= I2C_CR2_STOP;
+
+    // 2. Wait until STOPF flag is (re???)set by checking the same flag in ISR.
+    while (!(I2C2->ISR & I2C_ISR_STOPF));
+
+    // 3. Clear the STOPF flag by writing 1 to the corresponding bit in the ICR.
+    I2C2->ICR |= I2C_ICR_STOPCF;
+}
+
+void i2c_waitidle(void) {
+    // nano_wait(1000000000000);
+    while (I2C2->ISR & I2C_ISR_BUSY);
+}
+
+int8_t i2c_senddata(uint8_t targadr, uint8_t data[], uint8_t size) {
+    
+    // printf("in senddata\n");
+    // 1. Wait until the I2C bus is idle
+    i2c_waitidle();
+    // printf("WRITE now idle\n");
+
+    // printf("not idle\n");
+    // 2. Send a START condition with the target address and write bit
+    i2c_start(targadr, size, 0);  // 0 indicates a write operation
+
+    // printf("WRITE start bit sent\n");
+
+    for (uint8_t i = 0; i < size; i++) {
+
+        int count = 0;
+        while ((I2C2->ISR & I2C_ISR_TXIS) == 0) {
+            count += 1;
+            if (count > 1000000) {
+                printf("SEND timeout\n");
+                return -1;
+            }
+            if (i2c_checknack()) {
+                printf("SEND first nack\n");
+                i2c_clearnack();
+                i2c_stop();
+                return -1;
+            }
+        }
+        // volatile char curr = data[i];
+
+        printf("Writing (%d) %d (%c)\n", i, data[i] & I2C_TXDR_TXDATA, (char)(data[i] & I2C_TXDR_TXDATA));
+        // printf("\n");
+        I2C2->TXDR = data[i] & I2C_TXDR_TXDATA;
+        // printf("writing %d (%c)\n", data[i], data[i]);
+    }
+
+    // printf("through loop\n");
+
+    while (!(I2C2->ISR & I2C_ISR_TC) && !(I2C2->ISR & I2C_ISR_NACKF));
+
+    // printf("TC and NAKKF flags not set\n");
+
+    if (I2C2->ISR & I2C_ISR_NACKF) {
+        printf("SEND last nack\n");
+        // i2c_stop();
+        // i2c_clearnack();
+        return -1;  // No acknowledgment error
+    }
+
+    i2c_stop();
+
+    return 0;
+}
+
+int i2c_recvdata(uint8_t targadr, uint8_t *data, uint8_t size) {
+
+    // printf("here\n");
+    // uint8_t *byte_data = (uint8_t*)data;  // Cast void* to uint8_t* for byte-wise access
+
+    i2c_waitidle();
+    // printf("READ now idle\n");
+
+    // 2. Send a START condition with the target address and read bit (1)
+    i2c_start(targadr, size, 1);  // 1 indicates a read operation
+    // printf("READ start bit sent\n");
+
+    // 3. Loop through each byte of data to receive
+    for (uint8_t i = 0; i < size; i++) {
+        // Wait until RXNE flag is set, or timeout if it takes too long
+        int count = 0;
+        while ((I2C2->ISR & I2C_ISR_RXNE) == 0) {
+            count++;
+            if (count > 1000000) {
+                printf("RECV timeout\n");
+                return -1;  // Timeout error
+            }
+            if (i2c_checknack()) {
+                printf("RECV nack\n");
+                i2c_clearnack();
+                i2c_stop();
+                return -1;  // NACK error
+            }
+        }
+
+        // 4. Mask the received data in RXDR with I2C_RXDR_RXDATA to ensure it is 8 bits,
+        //    then store it in the data buffer
+        // byte_data[i] = I2C2->RXDR & I2C_RXDR_RXDATA;
+        data[i] = I2C2->RXDR & I2C_RXDR_RXDATA;
+
+        // printf("\n");
+        printf("Reading (%d) %d (%c)\n", i, data[i], (char)(data[i]));
+    }
+
+    while(!(I2C2->ISR & I2C_ISR_TC) && !(I2C2->ISR & I2C_ISR_NACKF));
+
+    if (!((I2C2->ISR & I2C_ISR_NACKF) == 0)) {
+        return -1;
+    }
+    // // 5. Send a STOP condition after receiving all the data
+    i2c_stop();
+
+    return 0;  // Success
+}
+
+void i2c_clearnack(void) {
+    I2C2->ICR |= I2C_ICR_NACKCF;
+}
+
+int i2c_checknack(void) {
+    if (I2C2->ISR & I2C_ISR_NACKF) {
+        return 1;  // NACK received
+    }
+    return 0;  // No NACK
+}
+
+#define EEPROM_ADDR 0x57
+
+void eeprom_write(uint16_t loc, const char* data, uint8_t len) {
+    printf("Starting Write\n");
+    // printf("in eeprom write: loc: %d, data: %s, len: %d\n", loc, data, len);
+    uint8_t bytes[34];
+    bytes[0] = loc>>8;
+    bytes[1] = loc&0xFF;
+    for(int i = 0; i<len; i++){
+        bytes[i+2] = data[i];
+    }
+    i2c_senddata(EEPROM_ADDR, bytes, len+2);
+    printf("Done Writing\n");
+}
+
+void eeprom_read(uint16_t loc, char data[], uint8_t len) {
+    // ... your code here
+    // printf("in eeprom read: loc: %d, %s, len: %d\n", loc, data, len);
+    printf("Starting Read\n");
+    uint8_t bytes[2];
+    bytes[0] = loc>>8;
+    bytes[1] = loc&0xFF;
+    i2c_senddata(EEPROM_ADDR, bytes, 2);
+    i2c_recvdata(EEPROM_ADDR, data, len);
+    printf("Done Reading\n");
+}
+
+// // end I2C stuff:
+
+
 
 
 uint8_t col; // the column being scanned
@@ -331,10 +564,26 @@ int read_matrix(int rgb)
 int main(void) {
     internal_clock();
     enable_ports();
+    init_i2c();
     init_tim7();
     char event;
     init_spi1();
     spi1_init_oled();
+
+    // // I2C test
+
+    char string[6] = {'t','e','s','t','x', '\0'};
+    // int len = strlen(string);
+    int len = 6;
+    // string[0] = (char)(len + 48);
+    uint16_t addr = 0;
+    eeprom_write(addr, string, len);
+    nano_wait(10000000);
+    char dataAd[6] = {0};
+    eeprom_read(addr, dataAd, len);
+
+    // // end I2C test
+
     char myString [] = {'x', '=', '0', '0', ' ', 'y', '=', '0', '0', '\0'};
     
     for(;;)
@@ -351,7 +600,8 @@ int main(void) {
         
         write_matrix(1, 0, 1);
         char rgb[] = {'r', '=', (char)(read_matrix(0) + 48), ' ', 'g', '=', (char)(read_matrix(1) + 48), 'b', '=', (char)(read_matrix(2) + 48)};
-        spi1_display1(rgb);
+        // spi1_display1(rgb);
+        // spi1_display1(dataAd);
         myString[3] = (char)(curX % 10 + 48);
         myString[2] = (char)(curX / 10 + 48);
         myString[8] = (char)(curY % 10 + 48);
